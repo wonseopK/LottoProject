303526694f0e9f49acba9d94e429487a2d1f9760
josh <josh@myfair.co>
2022-02-08 18:16:02 +0900

    init

---
+++ src/main/java/lotto/WebUILottoApplication.java
@@ -0,0 +1,15 @@
+package lotto;
+
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.get;
+
+public class WebUILottoApplication {
+    public static void main(String[] args) {
+        // TODO: 2022/02/08 todo 진행
+    }
+}
