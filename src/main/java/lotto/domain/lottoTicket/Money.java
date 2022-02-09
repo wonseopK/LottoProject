b4a1fb5c860fa0497e1b43e3c875871a4a9802a1
josh <josh@myfair.co>
2022-02-09 14:10:00 +0900

    feat 머니클래스 구현

--- src/main/java/lotto/domain/lottoTicket/Money.java
+++ src/main/java/lotto/domain/lottoTicket/Money.java
@@ -1,4 +1,21 @@
 package lotto.domain.lottoTicket;
 
+import lotto.domain.validator.lottoTicket.MoneyValidator;
+
 public class Money {
+    private long money;
+
+    private Money(final long money) {
+        MoneyValidator.checkIsValid(money);
+        this.money = money;
+    }
+
+    public static Money create(final long inputMoney) {
+        return new Money(inputMoney);
+    }
+
+    public long getMoney() {
+        return money;
+    }
+
 }
