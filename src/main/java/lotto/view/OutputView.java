4fdc0c7b3e351d8e42e67c7f3f05ac956fbd81ff
josh <josh@myfair.co>
2022-02-09 11:25:44 +0900

    feat 생성자를 사용하지 못하도록 하고 프린트 메서드 생성

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,4 +1,10 @@
 package lotto.view;
 
 public class OutputView {
+    private OutputView() {
+    }
+
+    public static void println(final String msg) {
+        System.out.println(msg);
+    }
 }
