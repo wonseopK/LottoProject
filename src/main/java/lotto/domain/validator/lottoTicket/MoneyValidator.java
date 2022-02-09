00a77c2fa548818e37ba49a47e33b6e22fad3685
josh <josh@myfair.co>
2022-02-09 14:11:29 +0900

    feat 입력한 금액의 유효성 검사

--- src/main/java/lotto/domain/validator/lottoTicket/MoneyValidator.java
+++ src/main/java/lotto/domain/validator/lottoTicket/MoneyValidator.java
@@ -1,4 +1,16 @@
 package lotto.domain.validator.lottoTicket;
 
 public class MoneyValidator {
+    private MoneyValidator() {
+    }
+
+    public static void checkIsValid(long money) {
+        if (!checkIsPositive(money)) {
+            throw new IllegalArgumentException("0 보다 큰 금액을 입력해주세요");
+        }
+    }
+
+    public static boolean checkIsPositive(long money) {
+        return money > 0;
+    }
 }
