662c2e30732ab0bf4308d82264271a997bd10d68
josh <josh@myfair.co>
2022-02-09 14:11:12 +0900

    feat 금액 입력 메서드 생성

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,5 +1,28 @@
 package lotto.view;
 
-public class InputView {
+import lotto.domain.lottoTicket.Money;
+import lotto.domain.lottoTicket.TicketGenerateCount;
 
+import java.util.Scanner;
+
+public class InputView {
+    static final Scanner scanner = new Scanner(System.in);
+
+    private InputView() {
+    }
+
+    public static TicketGenerateCount inputMoney() {
+        OutputView.println("구입 금액을 입력해주세요");
+        Money inputMoney = Money.create(scanner.nextInt());
+        final long ticketMoney = inputMoney.getMoney();
+        return TicketGenerateCount.create(ticketMoney);
+    }
+
+//    public static int inputWinnerNumber(final Scanner scanner) {
+//        OutputView.println("지난주 당첨 번호를 ','로 구분하여 입력해주세요");
+//    }
+//
+//    public static int inputBonusNumber(final Scanner scanner) {
+//        OutputView.println("보너스 금액을 입력해주세요");
+//    }
 }
