9656e29b4ab8237b0ae693e897545b34ccbd8515
josh <josh@myfair.co>
2022-02-09 19:04:51 +0900

    [실패한테스]: String으로 입력된 숫자를 ,기준으로 분리해주는 기능

--- src/main/java/lotto/domain/utils/ResultNumberHandler.java
+++ src/main/java/lotto/domain/utils/ResultNumberHandler.java
@@ -1,4 +1,17 @@
 package lotto.domain.utils;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class ResultNumberHandler {
+    private static final String NUMBER_SPERATOR = ",";
+
+    private ResultNumberHandler() {
+    }
+
+    public static List<Integer> splitResultNumbers(String resultNumbers) {
+        return new ArrayList<>();
+    }
 }
---
+++ src/test/java/lotto/domain/utils/ResultNumberHandlerTest.java
@@ -0,0 +1,26 @@
+package lotto.domain.utils;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+class ResultNumberHandlerTest {
+    @DisplayName(",로 구분하여 입력한숫자를 List로 반환해주는 기능")
+    @Test
+    void name() {
+        //given
+        String inputNumbers = "1,2,3,4,5";
+
+        //when
+        List<Integer> winNumbers = ResultNumberHandler.splitResultNumbers(inputNumbers);
+
+        //then
+        Assertions.assertThat(winNumbers.get(0)).isEqualTo(1);
+        Assertions.assertThat(winNumbers.get(1)).isEqualTo(2);
+        Assertions.assertThat(winNumbers.get(2)).isEqualTo(3);
+        Assertions.assertThat(winNumbers.get(3)).isEqualTo(4);
+        Assertions.assertThat(winNumbers.get(4)).isEqualTo(5);
+    }
+}
