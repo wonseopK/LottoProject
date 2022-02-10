f1980bf422f2dfbc30dad31cd15633fa2639378d
josh <josh@myfair.co>
2022-02-10 10:08:44 +0900

    [refactor]: 클래스 이름변경

--- src/main/java/lotto/domain/utils/WinnerNumberHandler.java
+++ src/main/java/lotto/domain/utils/WinnerNumberHandler.java
@@ -4,15 +4,16 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class ResultNumberHandler {
+public class WinnerNumberHandler {
     private static final String NUMBER_SPERATOR = ",";
 
-    private ResultNumberHandler() {
+    private WinnerNumberHandler() {
     }
 
     public static List<Integer> splitResultNumbers(String resultNumbers) {
         return Arrays.stream(resultNumbers.split(NUMBER_SPERATOR))
                 .map(number -> Integer.valueOf(number.trim()))
+                .sorted()
                 .collect(Collectors.toList());
     }
 }
