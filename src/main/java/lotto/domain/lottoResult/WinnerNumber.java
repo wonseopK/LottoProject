f228769e44249de4e35dfbf5529e72440d34662b
josh <josh@myfair.co>
2022-02-10 22:57:53 +0900

    [refactor] 당첨번호와 보너스번호 중복 제거 기능추가

--- src/main/java/lotto/domain/lottoResult/WinnerNumber.java
+++ src/main/java/lotto/domain/lottoResult/WinnerNumber.java
@@ -10,8 +10,16 @@ public class WinnerNumber {
 
     public WinnerNumber(List<Integer> winnerNumbers, BonusNumber bonusNumber) {
         WinnerNumberValidator.checkIsValid(winnerNumbers);
+        validateBonusNumberNotContainWinnerNumber(winnerNumbers, bonusNumber);
         this.winnerNumbers = winnerNumbers;
         this.bonusNumber = bonusNumber;
+    }
+
+    private void validateBonusNumberNotContainWinnerNumber(List<Integer> winnerNumbers, BonusNumber bonusNumber) {
+        if (winnerNumbers.contains(bonusNumber.getBonusNumber())) {
+            throw new IllegalArgumentException("당첨번호에 중복되는 숫자입니다.");
+        }
+
     }
 
     public List<Integer> getWinnerNumbers() {
