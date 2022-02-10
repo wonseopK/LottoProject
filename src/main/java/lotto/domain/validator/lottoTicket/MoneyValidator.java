54535f4e4c05d29706d1535ca745ef7dd53b114e
josh <josh@myfair.co>
2022-02-10 22:55:46 +0900

    [refactor] 최소금액 1000원으로 변경

--- src/main/java/lotto/domain/validator/lottoTicket/MoneyValidator.java
+++ src/main/java/lotto/domain/validator/lottoTicket/MoneyValidator.java
@@ -5,12 +5,12 @@ public class MoneyValidator {
     }
 
     public static void checkIsValid(long money) {
-        if (isUnderZero(money)) {
-            throw new IllegalArgumentException("0 보다 큰 금액을 입력해주세요");
+        if (isUnderTicketPrice(money)) {
+            throw new IllegalArgumentException("1000원 보다 큰 금액을 입력해주세요");
         }
     }
 
-    private static boolean isUnderZero(long money) {
-        return money <= 0;
+    private static boolean isUnderTicketPrice(long money) {
+        return money <= 1000;
     }
 }
