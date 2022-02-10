e9ab399f217137a4c52c8f49b2e8e52086d43f38
josh <josh@myfair.co>
2022-02-10 09:49:44 +0900

    [feat]: 주문한 티켓 출력 해주는 메서드 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,11 +1,21 @@
 package lotto.view;
 
+import lotto.domain.lottoTicket.LottoTicket;
+import lotto.domain.lottoTicket.LottoTickets;
+
 public class OutputView {
     private OutputView() {
     }
 
     public static void println(final String msg) {
         System.out.println(msg);
+    }
+
+    public static void printOrderedTickets(LottoTickets lottoTickets) {
+        for (LottoTicket lottoTicket : lottoTickets.getLottoTickets()) {
+            OutputView.println(lottoTicket.toString());
+        }
+
     }
 
 }
--- src/test/java/lotto/domain/lottoTicket/LottoServiceTest.java
+++ src/test/java/lotto/domain/lottoTicket/LottoServiceTest.java
@@ -1,5 +1,6 @@
 package lotto.domain.lottoTicket;
 
+import lotto.view.OutputView;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
@@ -9,17 +10,14 @@ class LottoServiceTest {
     @DisplayName("6자리 로또 번호 생성 기능")
     @Test
     void name() {
-        LottoTickets lottoTickets = LottoService.giveOrderedTicket(1);
+        LottoTickets lottoTickets = LottoService.giveOrderedTicket(5);
         long numberSize = lottoTickets.getLottoTickets().get(0).getLottoNumbers().size();
         long distinctedNumberSize = lottoTickets.getLottoTickets().get(0).getLottoNumbers().stream().distinct().count();
         boolean isDuplicated = distinctedNumberSize != numberSize;
 
         Assertions.assertThat(isDuplicated).isEqualTo(false);
         Assertions.assertThat(numberSize).isEqualTo(6);
-        System.out.println(lottoTickets.getLottoTickets().get(0).toString());
-
-
-
+        OutputView.printOrderedTickets(lottoTickets);
 
     }
 
@@ -27,7 +25,6 @@ class LottoServiceTest {
     @RepeatedTest(30)
     void name2() {
         LottoTickets lottoTickets = LottoService.giveOrderedTicket(1);
-        System.out.println(lottoTickets.getLottoTickets().get(0).getLottoNumbers().toString());
         for (Integer number : lottoTickets.getLottoTickets().get(0).getLottoNumbers()) {
             Assertions.assertThat(number).isBetween(1, 45);
         }
