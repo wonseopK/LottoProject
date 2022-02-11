177be8c501c4ff42abb6b9402e26530d736b82cd
josh <josh@myfair.co>
2022-02-11 15:20:08 +0900

    [refactor] 로또 티켓 validator 분리

--- src/main/java/lotto/domain/lottoTicket/LottoTicket.java
+++ src/main/java/lotto/domain/lottoTicket/LottoTicket.java
@@ -1,34 +1,16 @@
 package lotto.domain.lottoTicket;
 
+import lotto.domain.validator.lottoTicket.LottoTicketValidator;
+
 import java.util.List;
 
 public class LottoTicket {
-    public static final int LOTTO_NUMBER_SIZE = 6;
-
     private final List<Integer> lottoNumbers;
 
     public LottoTicket(List<Integer> lottoNumbers) {
-        validateSize(lottoNumbers);
-        validateDuplicate(lottoNumbers);
+        LottoTicketValidator.validateSize(lottoNumbers);
+        LottoTicketValidator.validateDuplicate(lottoNumbers);
         this.lottoNumbers = lottoNumbers;
-    }
-
-    private void validateSize(List<Integer> lottoNumbers) {
-        boolean isNotValidNumbers = lottoNumbers.size() != LOTTO_NUMBER_SIZE;
-        if (isNotValidNumbers) {
-            throw new IllegalArgumentException("로또 번호 길이가 6자리가 아닙니다.");
-        }
-    }
-
-    private void validateDuplicate(List<Integer> lottoNumbers) {
-        long nonDuplicateNumbers = lottoNumbers
-                .stream()
-                .distinct()
-                .count();
-        boolean isDuplictednumbers = nonDuplicateNumbers != LOTTO_NUMBER_SIZE;
-        if (isDuplictednumbers) {
-            throw new IllegalArgumentException("로또 번호는 중복 될수 없습니다.");
-        }
     }
 
     @Override
