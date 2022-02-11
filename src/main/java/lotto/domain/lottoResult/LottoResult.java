68397012faf715b807faeda3bdd61cbd2f39282c
josh <josh@myfair.co>
2022-02-11 15:17:10 +0900

    [refactor] 사용하지 않는 메서드 제거

--- src/main/java/lotto/domain/lottoResult/LottoResult.java
+++ src/main/java/lotto/domain/lottoResult/LottoResult.java
@@ -1,35 +1,18 @@
 package lotto.domain.lottoResult;
 
-import java.util.ArrayList;
-import java.util.List;
+import lotto.domain.lottoTicket.Money;
+
 import java.util.Map;
 
 public class LottoResult {
-    private static final int DEFAULT_VALUE = 0;
+    private static final long DEFAULT_VALUE = 0;
 
-    private final Map<Integer, Integer> result;
+    private final Map<String, Long> result;
+    private final double totalPrizeMoney;
 
-    public LottoResult(Map<Integer, Integer> result) {
+    public LottoResult(Map<String, Long> result, long totalPrizeMoney) {
         this.result = result;
-    }
-
-    public long calculateTotalWinningMoney() {
-        List<Long> winMoneyLevel = winMoneyLevel();
-        long totalWinningMoney = 0;
-        for (int i = 3; i < 8; i++) {
-            totalWinningMoney += result.get(i) * winMoneyLevel.get(i-3);
-        }
-        return totalWinningMoney;
-    }
-
-    private static List<Long> winMoneyLevel() {
-        List<Long> winMoneyLevel = new ArrayList<>();
-        winMoneyLevel.add(5000L);
-        winMoneyLevel.add(50000L);
-        winMoneyLevel.add(1500000L);
-        winMoneyLevel.add(2000000000L);
-        winMoneyLevel.add(30000000L);
-        return winMoneyLevel;
+        this.totalPrizeMoney = totalPrizeMoney;
     }
 
     @Override
@@ -39,15 +22,20 @@ public class LottoResult {
                         "5개 일치 (1500000원)- %d개\n" +
                         "5개 일치, 보너스 볼 일치(30000000원) - %d개\n" +
                         "6개 일치 (2000000000원)- %d개",
-                result.getOrDefault(3, DEFAULT_VALUE),
-                result.getOrDefault(4, DEFAULT_VALUE),
-                result.getOrDefault(5, DEFAULT_VALUE),
-                result.getOrDefault(7, DEFAULT_VALUE),
-                result.getOrDefault(6, DEFAULT_VALUE));
+                result.getOrDefault("FOURTH", DEFAULT_VALUE),
+                result.getOrDefault("THIRD", DEFAULT_VALUE),
+                result.getOrDefault("SECOND", DEFAULT_VALUE),
+                result.getOrDefault("BONUS", DEFAULT_VALUE),
+                result.getOrDefault("FIRST", DEFAULT_VALUE));
 
     }
 
-    public Map<Integer, Integer> getResult() {
+    public String calculateProfit(Money spendMoneye) {
+        double profit = (totalPrizeMoney / spendMoneye.getMoney());
+        return String.format("총 수익률은 %.2f입니다.", profit);
+    }
+
+    public Map<String, Long> getResult() {
         return result;
     }
 }
