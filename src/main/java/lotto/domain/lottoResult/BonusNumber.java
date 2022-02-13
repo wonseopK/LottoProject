94b716d9174a58044e9552ce4c2cecf6872a2591
josh <josh@myfair.co>
2022-02-13 17:16:21 +0900

    [refactor]:사용하지 않는 기능제거

--- src/main/java/lotto/domain/lottoResult/BonusNumber.java
+++ src/main/java/lotto/domain/lottoResult/BonusNumber.java
@@ -1,14 +1,11 @@
 package lotto.domain.lottoResult;
 
 import lotto.domain.lottoNumber.LottoNumber;
-import lotto.domain.lottoNumber.LottoNumberBox;
-import lotto.domain.validator.lottoResult.BonusNumberValidator;
 
 public class BonusNumber {
     private LottoNumber bonusNumber;
 
     public BonusNumber(LottoNumber bonusNumber) {
-//        BonusNumberValidator.validateBonusNumber(bonusNumber);
         this.bonusNumber = bonusNumber;
     }
 
