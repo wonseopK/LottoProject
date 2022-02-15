84626536bffc7005d6df0c9b27a5f3e48dcd8c91
josh <josh@myfair.co>
2022-02-15 14:33:09 +0900

    Refactor: 사용하지 않는 메서드 제거

--- src/main/java/lotto/domain/lottoTicket/LottoTickets.java
+++ src/main/java/lotto/domain/lottoTicket/LottoTickets.java
@@ -1,11 +1,9 @@
 package lotto.domain.lottoTicket;
 
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class LottoTickets {
-    private List<LottoTicket> lottoTickets;
+    private final List<LottoTicket> lottoTickets;
 
     public LottoTickets(List<LottoTicket> lottoTickets) {
         this.lottoTickets = lottoTickets;
@@ -13,10 +11,5 @@ public class LottoTickets {
 
     public List<LottoTicket> getLottoTickets() {
         return lottoTickets;
-    }
-
-    public static List<LottoTicket> lottoTicketsJoiner(List<LottoTicket> tickets1, List<LottoTicket> tickets2){
-        List<LottoTicket> joineTickets = Stream.concat(tickets1.stream(), tickets2.stream()).collect(Collectors.toList());;
-        return joineTickets;
     }
 }
