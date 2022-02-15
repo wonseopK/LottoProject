097b16f06b384d4678aee262bda3d025670e0391
josh <josh@myfair.co>
2022-02-15 16:37:06 +0900

    [실패한테스트] 로또머신 생성시 매개변수에 생성된 머신이 없을 경우 예외를 발생하는 기능

--- src/test/java/lotto/domain/lottoTicket/lottoMachine/LottoMachinesTest.java
+++ src/test/java/lotto/domain/lottoTicket/lottoMachine/LottoMachinesTest.java
@@ -6,6 +6,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -15,12 +16,22 @@ class LottoMachinesTest {
     @Test
     void name() {
         Money money = Money.create(3000);
-        LottoMachines lottoMachines = new LottoMachines();
-        lottoMachines.addLottoMachine(new RandomLottoMachine());
+        List<LottoMachine> lottoMachinesContainer = new ArrayList<>();
+        lottoMachinesContainer.add(new RandomLottoMachine());
+
+        LottoMachines lottoMachines = new LottoMachines(lottoMachinesContainer);
         List<LottoTicket> lottoTickets = lottoMachines.makeTickets(money);
 
         Assertions.assertThat(lottoTickets.size()).isEqualTo(3);
 
+    }
 
+    @DisplayName("로또머신 생성시 매개변수에 생성된 머신들이 없을때 생성자 생성이 불가능하도록 검사하는 기능 테스트")
+    @Test
+    void name2() {
+        List<LottoMachine> lottoMachineContainer = new ArrayList<>();
+
+        Assertions.assertThatThrownBy(()->new LottoMachines(lottoMachineContainer))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
