4e7f41e189ff2d780d0a90ab38ad43155d36630e
josh <josh@myfair.co>
2022-02-15 12:48:56 +0900

    [Refactor]: 패키지 이동

--- src/test/java/lotto/domain/lottoTicket/lottoNumber/LottoNumberBoxTest.java
+++ src/test/java/lotto/domain/lottoTicket/lottoNumber/LottoNumberBoxTest.java
@@ -14,4 +14,15 @@ class LottoNumberBoxTest {
 
         Assertions.assertThat(lottoNumber.getNumber()).isEqualTo(3);
     }
+    @DisplayName("1~45자리의 숫자만 요청할수 있도록 검사해주는 테스트")
+    @Test
+    void name2() {
+        int input1 = 46;
+        int input2 = 0;
+
+        Assertions.assertThatThrownBy(()-> LottoNumberBox.getLottoNumber(input1))
+                .isInstanceOf(IllegalArgumentException.class).hasMessage("1~45사이의 숫자만 꺼내올수있습니다.");
+        Assertions.assertThatThrownBy(()->LottoNumberBox.getLottoNumber(input2))
+                .isInstanceOf(IllegalArgumentException.class).hasMessage("1~45사이의 숫자만 꺼내올수있습니다.");
+    }
 }
