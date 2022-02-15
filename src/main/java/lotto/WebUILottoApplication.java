94828f04a88f2e3f9fad1c1ca32e25c7dde64ee9
josh <josh@myfair.co>
2022-02-15 10:42:24 +0900

    [refactor]: controller 로 분리

---
+++ src/main/java/lotto/WebUILottoApplication.java
@@ -0,0 +1,20 @@
+package lotto;
+
+import lotto.controller.LottoController;
+import lotto.domain.lottoTicket.LottoTickets;
+import lotto.domain.money.Money;
+import lotto.view.InputView;
+
+public class WebUILottoApplication {
+
+
+    public static void main(String[] args) {
+        // TODO: 2022/02/08 todo 진행
+        Money spendMoney = InputView.inputMoney();
+        int manualTicketCount = InputView.getManualTicketCount(spendMoney);
+
+        LottoController lottoController = new LottoController(spendMoney, manualTicketCount);
+        lottoController.run();
+
+    }
+}
---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,55 @@
+package lotto.controller;
+
+import lotto.domain.lottoResult.LottoResult;
+import lotto.domain.lottoResult.LottoResultFactory;
+import lotto.domain.lottoResult.WinnerNumber;
+import lotto.domain.lottoTicket.LottoTicket;
+import lotto.domain.lottoTicket.LottoTickets;
+import lotto.domain.lottoTicket.lottoMachine.LottoMachine;
+import lotto.domain.lottoTicket.lottoMachine.ManualLottoMahcine;
+import lotto.domain.lottoTicket.lottoMachine.RandomLottoMachine;
+import lotto.domain.money.Money;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+import java.util.List;
+
+public class LottoController {
+    private final Money spendMoney;
+    private final int manualTicketCount;
+
+    public LottoController(Money spendMoney, int manualTicketCount) {
+        this.spendMoney = spendMoney;
+        this.manualTicketCount = manualTicketCount;
+    }
+
+    public void run() {
+        LottoTickets lottoTickets = makeTickets();
+
+        WinnerNumber winnerNumber = InputView.inputWinnerNumber();
+        LottoResult lottoResult = LottoResultFactory.getLottoResult(winnerNumber, lottoTickets);
+
+        OutputView.printLottoResult(lottoResult, spendMoney);
+    }
+
+    private LottoTickets makeTickets() {
+        LottoTickets lottoTickets = useMachinesToMakeTickets(manualTicketCount, spendMoney);
+        OutputView.printOrderType(spendMoney, manualTicketCount);
+        OutputView.printOrderTickets(lottoTickets);
+        return lottoTickets;
+    }
+
+    private LottoTickets useMachinesToMakeTickets(int manualTicketCount, Money spendMoney){
+        LottoMachine manualMachine = new ManualLottoMahcine(manualTicketCount);
+        LottoMachine randomMahcine = new RandomLottoMachine();
+
+        List<LottoTicket> manualLottoTickets = manualMachine.buyTickets(spendMoney);
+        List<LottoTicket> autoLottoTickets = randomMahcine.buyTickets(spendMoney);
+
+        List<LottoTicket> joinLottoTickets = LottoTickets.lottoTicketsJoiner(manualLottoTickets, autoLottoTickets);
+        return new LottoTickets(joinLottoTickets);
+    }
+
+
+
+}
