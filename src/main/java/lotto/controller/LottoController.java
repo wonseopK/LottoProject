e9d7467650857fca6dfa63ac2c0f93ffb37fd49f
josh <josh@myfair.co>
2022-02-15 15:09:34 +0900

    Refactor: 티켓 구매 기능 변경

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -6,6 +6,7 @@ import lotto.domain.lottoResult.WinnerNumber;
 import lotto.domain.lottoTicket.LottoTicket;
 import lotto.domain.lottoTicket.LottoTickets;
 import lotto.domain.lottoTicket.lottoMachine.LottoMachine;
+import lotto.domain.lottoTicket.lottoMachine.LottoMachines;
 import lotto.domain.lottoTicket.lottoMachine.ManualLottoMahcine;
 import lotto.domain.lottoTicket.lottoMachine.RandomLottoMachine;
 import lotto.domain.money.Money;
@@ -16,11 +17,9 @@ import java.util.List;
 
 public class LottoController {
     private final Money spendMoney;
-    private final int manualTicketCount;
 
-    public LottoController(Money spendMoney, int manualTicketCount) {
+    public LottoController(Money spendMoney) {
         this.spendMoney = spendMoney;
-        this.manualTicketCount = manualTicketCount;
     }
 
     public void run() {
@@ -34,26 +33,16 @@ public class LottoController {
 
     private LottoTickets makeTickets() {
         LottoTickets lottoTickets = useMachinesToMakeTickets();
-        OutputView.printAutoOrderCount(spendMoney);
         OutputView.printOrderTickets(lottoTickets);
         return lottoTickets;
     }
 
     private LottoTickets useMachinesToMakeTickets(){
-
-
-        LottoMachine manualMachine = new ManualLottoMahcine(manualTicketCount, spendMoney);
-        LottoMachine randomMahcine = new RandomLottoMachine(spendMoney);
-
-
-        List<LottoTicket> manualLottoTickets = manualMachine.buyTickets();
-        List<LottoTicket> autoLottoTickets = randomMahcine.buyTickets();
-
-
-        List<LottoTicket> joinLottoTickets = LottoTickets.lottoTicketsJoiner(manualLottoTickets, autoLottoTickets);
-        return new LottoTickets(joinLottoTickets);
+        LottoMachines lottoMachines = new LottoMachines();
+        lottoMachines.addLottoMachine(new ManualLottoMahcine());
+        lottoMachines.addLottoMachine(new RandomLottoMachine());
+        List<LottoTicket> lottoTickets = lottoMachines.makeTickets(spendMoney);
+        return new LottoTickets(lottoTickets);
     }
-
-
 
 }
