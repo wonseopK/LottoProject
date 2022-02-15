8807b551d1e83c5ff8ab8061af80389e760df473
josh <josh@myfair.co>
2022-02-15 15:08:13 +0900

    Refactor: 생성자 제거

--- src/main/java/lotto/domain/lottoTicket/lottoMachine/RandomLottoMachine.java
+++ src/main/java/lotto/domain/lottoTicket/lottoMachine/RandomLottoMachine.java
@@ -3,20 +3,16 @@ package lotto.domain.lottoTicket.lottoMachine;
 import lotto.domain.lottoTicket.LottoTicket;
 import lotto.domain.lottoTicket.lottoNumber.LottoNumberBox;
 import lotto.domain.money.Money;
+import lotto.view.OutputView;
 
 import java.util.*;
 
 public class RandomLottoMachine implements LottoMachine {
-    private Money spendMoney;
-
-    public RandomLottoMachine(Money money) {
-        this.spendMoney = money;
-    }
-
     @Override
-    public List<LottoTicket> buyTickets() {
+    public List<LottoTicket> buyTickets(Money money) {
+        OutputView.printAutoOrderCount(money);
         List<LottoTicket> lottoTickets = new ArrayList<>();
-        for (int i = 0; i < spendMoney.getTicketCount(); i++) {
+        for (int i = 0; i < money.getTicketCount(); i++) {
             LottoTicket lottoTicket = new LottoTicket(LottoNumberBox.getSixRandomLottoNumbers());
             lottoTickets.add(lottoTicket);
         }
