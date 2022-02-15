3bd1ba9598cedcce72c30c122ef4344dfa2b8979
josh <josh@myfair.co>
2022-02-15 15:10:33 +0900

    Refactor: 사용하지 않는 메서드 제거

--- src/test/java/lotto/domain/lottoTicket/lottoMachine/LottoMachinesTest.java
+++ src/test/java/lotto/domain/lottoTicket/lottoMachine/LottoMachinesTest.java
@@ -13,15 +13,14 @@ import static org.junit.jupiter.api.Assertions.*;
 class LottoMachinesTest {
     @DisplayName("자동출력 머신 사용시 티켓 출력갯수 검사")
     @Test
-    void name () {
+    void name() {
         Money money = Money.create(3000);
         LottoMachines lottoMachines = new LottoMachines();
-        lottoMachines.addLottoMachine(new RandomLottoMachine(money));
-        System.out.println(lottoMachines.getLottoMachines().size());
-        List<LottoTicket> lottoTickets = lottoMachines.makeTickets();
+        lottoMachines.addLottoMachine(new RandomLottoMachine());
+        List<LottoTicket> lottoTickets = lottoMachines.makeTickets(money);
 
         Assertions.assertThat(lottoTickets.size()).isEqualTo(3);
 
 
     }
-}ㅇ
+}
